878551e7cec06ddc8a2f13881625f6926268d8bc
Choirong <c41980945@gmail.com>
2025-02-11 19:23:38 +0900

    JWT 시크릿키 로그 확인 코드 삭제

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const mongoose = require("mongoose");
 const authRoutes = require('./routes/auth');
